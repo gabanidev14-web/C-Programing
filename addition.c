350cf1aced5607ecf9a303a2bda1f490a2508404
gabanidev14-web <gabanidev14@gmail.com>
2025-08-08 10:57:03 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+int num1,num2,sum;
+
+printf("Enter first no :");
+scanf("%d", &num1);
+
+printf("Enter second no :");
+scanf("%d", &num2);
+
+sum= num1+num2;
+printf("The Sum is :%d" , sum);
+}
---
+++ subtraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+int no1,no2,ans;
+
+printf("enter first no :");
+scanf("%d", &no1);
+
+printf("enter second no :");
+scanf("%d", &no2);
+
+ans= no1-no2;
+printf("the subtraction is : %d", ans);
+}
