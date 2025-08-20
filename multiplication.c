510265a0553b2f65925e981dd4964734f6aa2c99
gabanidev14-web <gabanidev14@gmail.com>
2025-08-20 19:23:48 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+ {
+    int a,b,c;
+    printf("enter first no :\n");
+    scanf("%d", &a);
+    printf("enter second no :\n");
+    scanf("%d", &b);
+    c = a*b;
+    printf("multiplication of %d and %d is :%d",a,b,c);
+ }
